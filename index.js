1315768ae0ce0e31037c3428ba02dad27d2fde7e
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-01-14 21:42:46 +0100

    Add .create method

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 'use strict'
 
 var forEach = require('lodash.foreach')
+var captureStackTrace = require('capture-stack-trace')
+
+function inherits (ctor, superCtor) {
+  ctor.super_ = superCtor
+  ctor.prototype = Object.create(superCtor.prototype, {
+    constructor: {
+      value: ctor,
+      enumerable: false,
+      writable: true,
+      configurable: true
+    }
+  })
+}
 
 var FACTORY = {
   OBJECT: function (error, fields) {
@@ -8,38 +21,71 @@ var FACTORY = {
       error[property] = value
     })
 
-    if (typeof error.message === 'function')
-      error.message = error.message.call(error)
-
-    if (error.code)
-      error.message = error.code + ', ' + error.message
+    if (typeof error.message === 'function') error.message = error.message()
+    if (error.code) error.message = error.code + ', ' + error.message
 
     return error
   },
 
   STRING: function (error, args) {
-    var argsSize = {
+    var fn = {
       3: function () {
         error.name = args[0]
         error.code = args[1]
-        return error.message = error.code + ', ' + args[2]
+        error.message = error.code + ', ' + args[2]
+        return error.message
       },
       2: function () {
         error.name = args[0]
-        return error.message = args[1]
+        error.message = args[1]
+        return error.message
       },
       1: function () {
-        return error.message = args[0]
+        error.message = args[0]
+        return error.message
       },
       0: function () {}
     }
-    argsSize[args.length]()
+    fn[args.length]()
+    return error
+  },
+
+  STRING_NONAME: function (error, args) {
+    var fn = {
+      2: function () {
+        error.code = args[0]
+        error.message = error.code + ', ' + args[1]
+        return error.message
+      },
+      1: function () {
+        error.message = args[0]
+        return error.message
+      },
+      0: function () {}
+    }
+
+    fn[args.length]()
     return error
   }
 }
 
-module.exports = function (type) {
-  var error = Error()
-  if (typeof type === 'object') return FACTORY.OBJECT(error, type)
-  return FACTORY.STRING(error, arguments)
+function Factory (ErrorType, factoryString) {
+  return function (type) {
+    var error = new ErrorType()
+    if (typeof type === 'object') return FACTORY.OBJECT(error, type)
+    return factoryString(error, arguments)
+  }
+}
+
+module.exports = Factory(Error, FACTORY.STRING)
+
+module.exports.create = function (className) {
+  if (typeof className !== 'string') throw new TypeError('Expected className to be a string')
+  if (/[^0-9a-zA-Z_$]/.test(className)) throw new Error('className contains invalid characters')
+
+  var ErrorClass = eval('(function ' + className + '() { captureStackTrace(this, this.constructor); })')
+
+  inherits(ErrorClass, Error)
+  ErrorClass.prototype.name = className
+  return Factory(ErrorClass, FACTORY.STRING_NONAME)
 }
--- test/test.coffee
+++ test/test.coffee
@@ -4,14 +4,14 @@ isType = require 'is-type'
 
 describe 'Whoops ::', ->
 
-  describe 'without factory', ->
-    it 'basic', ->
-      err = Whoops()
-      isType.error(err).should.be.true()
-      err.name.should.be.equal 'Error'
-      (!!err.code).should.be.equal false
-      (!!err.description).should.be.equal false
-      (!!err.message).should.be.equal false
+  it 'basic', ->
+    err = Whoops()
+
+    isType.error(err).should.be.true()
+    err.name.should.be.equal 'Error'
+    (!!err.code).should.be.equal false
+    (!!err.description).should.be.equal false
+    (!!err.message).should.be.equal false
 
   describe 'factory', ->
 
@@ -71,19 +71,39 @@ describe 'Whoops ::', ->
     describe 'string', ->
       it 'providing message', ->
         err = Whoops 'damn'
+
         isType.error(err).should.be.true()
         err.name.should.be.equal 'Error'
         err.message.should.be.equal 'damn'
 
       it "providing mesage and name", ->
         err = Whoops 'DAMN', 'something is wrong'
+
         isType.error(err).should.be.true()
         err.name.should.be.equal 'DAMN'
         err.message.should.be.equal 'something is wrong'
 
       it "providing mesage, name and code", ->
         err = Whoops 'DAMN', 'ENOCODE', 'something is wrong'
+
         isType.error(err).should.be.true()
         err.name.should.be.equal 'DAMN'
+        err.code.should.be.equal 'ENOCODE'
+        err.message.should.be.equal 'ENOCODE, something is wrong'
+
+    describe  'class', ->
+      it 'create a custom class', ->
+        MyError = Whoops.create 'MyError'
+        err = MyError()
+
+        isType.error(err).should.be.true()
+        err.should.be.an.instanceof(Error)
+
+      it "avoid param 'name' from string factory", ->
+        MyError = Whoops.create 'MyError'
+        err = MyError('ENOCODE', 'something is wrong')
+
+        isType.error(err).should.be.true()
+        err.name.should.be.equal 'MyError'
         err.code.should.be.equal 'ENOCODE'
         err.message.should.be.equal 'ENOCODE, something is wrong'
